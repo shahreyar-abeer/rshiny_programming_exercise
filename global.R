818fb3481ed241f3ce00b1c7ed65e721894742ff
Zauad <shahreyar.abeer@gmail.com>
2020-04-14 21:27:17 +0600

    cleaned column names, added make_plot2()

--- global.R
+++ global.R
@@ -1,22 +1,51 @@
 
 
 ## required libraries
-library(tidyverse)
-library(ggcharts)
-library(gt)
-library(glue)
+req_libs = c("tidyverse", "gt", "janitor", "glue", "patchwork",
+             "ggeasy")
+pacman::p_load(char = req_libs)
 
 ## reading the data
 data_patient = read_tsv("./Random_PatientLevelInfo_2020.tsv")
 data_lab_vals = read_tsv("./Random_LabValuesInfo_2020.tsv")
 
+## fixing column names
+data_patient = data_patient %>% clean_names()
+data_lab_vals = data_lab_vals %>% clean_names()
+
 ## joining them
-data_merged = right_join(data_patient, data_lab_vals, by = c("USUBJID", "STUDYID"))
+data_merged = right_join(data_patient, data_lab_vals, by = c("usubjid", "studyid"))
 
 ## list of patients
-patient_list = unique(data_patient$USUBJID)
+patient_list = unique(data_patient$usubjid)
 
 ## list of tests
-test_list = unique(data_lab_vals$LBTESTCD)
+test_list = unique(data_lab_vals$lbtestcd)
+
+## function to make plot2
+make_plot2 = function(df, df2, test) {
+  remaining_tests = test_list[test_list != test]
+  #print(str(df))
+  p2 = df %>%
+    filter(lbtestcd == remaining_tests[1]) %>% 
+    ggplot(aes(x = avisit, y = aval, group = 1)) +
+    geom_path(color = "#1F77B4", size = .75) +
+    geom_point(size = 4, color = "#1F77B4") +
+    scale_x_discrete(limits = df2$avisit) +
+    ylim(0, 80) +
+    ggthemes::theme_pander() +
+    labs(title = glue("{remaining_tests[1]} test scores"))
+    p3 = df %>%
+      filter(lbtestcd == remaining_tests[2]) %>% 
+      ggplot(aes(x = avisit, y = aval, group = 1)) +
+      geom_path(color = "#1F77B4", size = .75) +
+      geom_point(size = 4, color = "#1F77B4") +
+      scale_x_discrete(limits = df2$avisit) +
+      ylim(0, 80) +
+      ggthemes::theme_pander() +
+      labs(title = glue("{remaining_tests[2]} test scores"))
+    
+    p2 | p3
+}
 
 
