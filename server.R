cca0a593b8f7577bd4b74effacf27b1da4057b5f
Zauad <shahreyar.abeer@gmail.com>
2020-04-14 14:13:19 +0600

    adding plot1

--- server.R
+++ server.R
@@ -3,8 +3,39 @@
 
 server = function(input, output) {
     
-    ## list of patients as a select input
+    ## select patient
     output$inp_patient = renderUI({
-        selectizeInput("patient", label = "Patient", choices = patient_list)
+        selectizeInput(inputId = "patient", label = "Patient ID", choices = patient_list)
+    })
+    
+    ## select test
+    output$inp_test = renderUI({
+        selectizeInput(inputId = "test", label = "Test", choices = test_list)
+    })
+    
+    ## a lollipop chart
+    output$plot1 = renderPlot({
+        d1 = data_merged %>%
+            filter(USUBJID == input$patient, LBTESTCD == input$test)
+        p1 = d1 %>% 
+            lollipop_chart(x = AVISIT, y = AVAL, horizontal = F, sort = F, point_color = "red") +
+            geom_hline(yintercept = input$threshold, color = "red", linetype = "longdash") +
+            gghighlight::gghighlight(AVAL > input$threshold,
+                                     unhighlighted_params = aes(colour = "#1F77B4"),
+                                     use_direct_label = F) +
+            scale_x_discrete(limits = d1$AVISIT)
+        p1
+    })
+    
+    ## info of the patient
+    output$patient_info = render_gt({
+        data_merged %>%
+            filter(USUBJID == input$patient, LBTESTCD == input$test) %>%
+            select(BMRKR1, BMRKR2, AGE, SEX, RACE, ACTARM, LBCAT) %>%
+            slice(1) %>% 
+            gt() %>%
+            tab_header(title = "Patient Details", subtitle = glue("Patient ID: {input$patient}")) %>% 
+            opt_table_lines()
+            
     })
 }
--- ui.R
+++ ui.R
@@ -10,10 +10,34 @@ ui = dashboardPage(
     ),
     
     dashboardSidebar(
-        uiOutput("inp_patient")
+        div(h4("Inputs"), align = "center"),
+        uiOutput("inp_patient"),
+        uiOutput("inp_test"),
+        numericInput("threshold", "Threshold", min = 0, max = 100, value = 50)
     ),
     
     dashboardBody(
-        
+        fluidRow(
+            tabBox(
+                id = "tabs",
+                width = 12,
+                tabPanel(
+                    title = "Plot1",
+                    fluidRow(
+                        column(
+                            width = 7,
+                            plotOutput("plot1")
+                        ),
+                        column(
+                            width = 5,
+                            gt_output("patient_info")
+                        )
+                    )
+                ),
+                tabPanel(
+                    title = "Plot2"
+                )
+            )
+        )
     )
 )
